e9205caf66cc94b470521cb3da9d33c8061d26f9
JesusDavidJaimesSerranoADSO <jdavidjaimess@gmail.com>
2023-03-22 07:19:19 -0500

    feat: :sparkles: sentencias condicionales
    utilizacion de las condicionales de v-if y el v-else-if

--- component/my-component.js
+++ component/my-component.js
@@ -1,7 +1,9 @@
 export default{
     data(){
         return{
-            centinela: true
+            opcion: ``,
+            mensaje1:"este es el mensaje abierto paara todo el mundo, que viva america",
+            mensaje2:"este es el mensaje ..."
         }
     }
 }
